1374831151f312ef6ba365ed079f4b1120cb7999
Jeremy Bobbin <jer@jer.cx>
2020-08-28 05:38:51 -0700

    passthrough POC passes input through command passed in args

--- sthkd.c
+++ sthkd.c
@@ -10,9 +10,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define MAX(a,b) (a) > (b) ? (a) : (b)
+
 struct termios orig, term;
-/* ipid: interpreter pid */
-int pty, pid, ipid;
+/* ipid, ipipe, opipe: interpreter {pid,{input,output} pipe} */
+int pty, pid, ipid, ipipe[2], opipe[2], nfds;
 
 void
 restore()
@@ -37,6 +39,9 @@ main(int argc, char *argv[])
 	int len; 
 	char buf[BUFSIZ], *shell;
 	struct winsize winsize;
+	
+	if (argc < 2)
+		die("set args");
 
 	if (tcgetattr(STDIN_FILENO, &orig) == -1) {
 		die("tcgetattr");
@@ -54,7 +59,7 @@ main(int argc, char *argv[])
 			setenv("STHKD", "true", 1);
 			if ((shell = getenv("SHELL")) == NULL)
 				shell = "/bin/sh";
-			execvp(shell, NULL);
+			execlp(shell, shell, NULL);
 			die("exec");
 			/* unreachable */
 	}
@@ -71,26 +76,34 @@ main(int argc, char *argv[])
 
 	fflush(stdout);
 
+	if (pipe(ipipe) == -1 || pipe(opipe) == -1)
+		die("interpreter pipe");
+
+	fprintf(stderr, "ifork\n");
 	switch (ipid = fork()) {
 		case -1:
 			die("fork");
 			/* unreachable */
 		case 0:  /* child */
 			setenv("STHKD", "interpreter", 1);
-			dup2(0);
-			execvp(argv[1], &argv[2]);
+			dup2(ipipe[0], 0);
+			dup2(opipe[1], 1);
+			execvp(argv[1], argv+1);
 			die("exec");
 			/* unreachable */
 	}
 
 	
+
+	nfds = MAX(pty, opipe[0]) + 1;
 	while (1) {
 		fd_set fds;
 		FD_ZERO(&fds);
 		FD_SET(STDIN_FILENO, &fds);
 		FD_SET(pty, &fds);
+		FD_SET(opipe[0], &fds);
 
-		if (select(pty+1, &fds, NULL, NULL, NULL) == -1) {
+		if (select(nfds, &fds, NULL, NULL, NULL) == -1) {
 			if (errno == EINTR)
 				continue;
 			die("select");
@@ -98,6 +111,15 @@ main(int argc, char *argv[])
 
 		if (FD_ISSET(STDIN_FILENO, &fds)) {
 			if ((len = read(0, buf, sizeof(buf))) < 0) {
+				die("read stdin");
+			}
+			if (write(ipipe[1], buf, len) < len)
+				die("\n\nwrite to pty\n\n");
+
+		}
+
+		if (FD_ISSET(opipe[0], &fds)) {
+			if ((len = read(opipe[0], buf, sizeof(buf))) < 0) {
 				die("read stdin");
 			}
 			if (write(pty, buf, len) < len)
@@ -108,7 +130,7 @@ main(int argc, char *argv[])
 			if ((len = read(pty, buf, sizeof(buf))) < 0) {
 				die("read stdin");
 			}
-			if (write(2, buf, len) < len)
+			if (write(1, buf, len) < len)
 				die("write stdout");
 		}
 	}
